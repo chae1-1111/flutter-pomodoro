8b54f60ac6617374e569b8ac3cf309a547c79cf2
chae1-1111 <dla4212@naver.com>
2023-02-18 12:03:27 +0900

    add pause timer feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
-
+  bool isRunning = false;
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -27,6 +27,16 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -56,8 +66,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
                   iconSize: 120,
                   color: Theme.of(context).cardColor,
-                  onPressed: onStartPressed,
-                  icon: const Icon(Icons.play_circle_outline),
+                  onPressed: isRunning ? onPausePressed : onStartPressed,
+                  icon: Icon(isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline),
                 ),
               ),
             ),
