baf78b2b57533d085ea105753efff7a8bf84c601
chae1-1111 <dla4212@naver.com>
2023-02-18 12:18:14 +0900

    add totalSecond formatter

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,11 +10,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
   bool isRunning = false;
+  int totalPomodoros = 0;
   late Timer timer;
 
   void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+      return;
+    }
     setState(() {
       totalSeconds = totalSeconds - 1;
     });
@@ -39,6 +50,12 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    var data = duration.toString().split(".").first.split(":");
+    return '${data[1]}:${data[2]}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "$totalSeconds",
+                format(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -100,7 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          "0",
+                          "$totalPomodoros",
                           style: TextStyle(
                             fontSize: 58,
                             color:
